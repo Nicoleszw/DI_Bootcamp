a08d0ee6bf42b88a335f5a8dff003bbc2ecfdfd9
¨nicoleszw¨ <nicoleszw23@gmail.com>
2025-02-02 14:45:58 +0200

    exercise.py

--- Week1/Day1/exercise.py
+++ Week1/Day1/exercise.py
@@ -1,3 +1,64 @@
-#hello 
+#Exercise 1 
 
+print("Hello world\n" * 4)
 
+#Exercise 2 
+
+print(int(99^3)*8)
+
+#Exercise 3 
+
+#False
+#True
+#False
+#False
+#False
+
+#Exercise 4
+
+computer_brand = "Apple"
+
+print(f"I have a {computer_brand} computer")
+
+#Exercise 5 
+
+name = "nicole"
+age = "24" 
+shoe_size = "38" 
+info = name + " " + age + " " + shoe_size
+print(info)
+
+#Exercise 6 
+
+a = 8 
+b = 6
+
+if a > b : 
+  print("Hello World") 
+
+  #Exercise 7 
+
+  number = input("Please give me a number:")
+
+  if int(number) % 2 == 0 : 
+    print("Your number is even!")
+  else :
+    print ("your number is odd!")
+
+#Exercise 8 
+
+user_name = input("What is your name?")
+
+if user_name == "Nicole" : 
+  print("We have the same name my love!")
+else :
+  print("Beatiful name!")
+
+#Exercise 9 
+
+height = int(input("What is your height?"))
+ 
+if height > 145 :
+ print("You are tall enough to ride!!")
+else  :
+ print ("Sorry, you would need to grow some more to ride:(")
