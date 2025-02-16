75f5044962b0d5779c2987ff838525017c8a099b
¨nicoleszw¨ <nicoleszw23@gmail.com>
2025-02-16 22:21:51 +0200

    Week3

---
+++ Week3/Day1/XP/xp.sql
@@ -0,0 +1,36 @@
+--this is a comment 
+--CREATE TABLE actors  (
+--actor_id SERIAL PRIMARY KEY, 
+--first_name VARCHAR (50) NOT NULL,
+--last_name VARCHAR(50) NOT NULL,
+--age DATE NOT NULL,
+--number_oscars SMALLINT NOT NULL)
+
+--SELECT * FROM actors
+
+--INSERT INTO actors (first_name, last_name, age, number_oscars)
+--VALUES ('Matt', 'Damon', '08/10/1970', 5)
+
+--INSERT INTO actors (first_name, last_name, age, number_oscars)
+--VALUES('George','Clooney','06/05/1961', 2),
+--('George','Clooney','06/05/1961', 2),
+--('George','Clooney','06/05/1961', 2);
+
+--INSERT INTO actors (first_name, last_name, age, number_oscars)
+--VALUES ('Natalie', 'Portman', '09/06/1981', 1)
+
+--INSERT INTO actors (first_name, last_name, age, number_oscars)
+--VALUES ('Meryl', 'Streep', '06/22/1949', 2)
+
+--INSERT INTO actors (first_name, last_name, age, number_oscars)
+--VALUES ('Jennifer', 'Lawrence', '08/15/1990', 1),
+--('Leonardo', 'DiCaprio', '11/11/1974', 2),
+--('Tom', 'Hanks', '07/09/1956', 3);
+
+--SELECT * FROM actors 
+
+--SELECT first_name FROM actors WHERE first_name = 'Matt'
+
+
+
+ 
